d485a9800eedcc9cc917652a7bd3917ac7c103c6
gretmn102 <erlgreywatson@gmail.com>
2021-07-21 02:34:14 +0300

    refactor: EmojiTagsEdit

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -165,36 +165,175 @@ module EmojiUploadForm =
             | InProgress -> spinner
         ]
 
-type EmojiTagsEditState =
-    | View
-    | Edit of {| Tags: InputTags.State; Submit: Deferred<Result<unit, unit>> |}
 
-type EmojiState =
-    {
-        Emoji: Emoji
-        EmojiTagsEditState: EmojiTagsEditState
-    }
+[<Literal>]
+let TagRoute = "tag"
+
+module EmojiTagsEdit =
+    type EmojiTagsEditState =
+        | View
+        | Edit of {| Tags: InputTags.State; Submit: Deferred<Result<unit, unit>> |}
+
+    type EmojiState =
+        {
+            Emoji: Emoji
+            EmojiTagsEditState: EmojiTagsEditState
+        }
+    let init emoji =
+        {
+            Emoji = emoji
+            EmojiTagsEditState = View
+        }
+    type Msg =
+        | ChangeToEditMode
+        | Submit
+        | SubmitResult of Result<Emoji, unit>
+        | InputTagsMsg of InputTags.Msg
+
+    let update (msg: Msg) (state: EmojiState) =
+        match msg with
+        | ChangeToEditMode ->
+            let state =
+                { state with
+                    EmojiTagsEditState =
+                        {|
+                            Tags = InputTags.init state.Emoji.Tags
+                            Submit = HasNotStartedYet |}
+                        |> Edit
+                }
+            state, Cmd.none
+        | Submit ->
+            let emoji = state
+            match emoji.EmojiTagsEditState with
+            | Edit edit ->
+                let em =
+                    { emoji.Emoji with
+                        Tags = edit.Tags.InputTagsState.Tags
+                    }
+                let cmd =
+                    Cmd.OfAsync.perform
+                        api.updateEmoji
+                        em
+                        (function
+                         | Ok () -> SubmitResult (Ok em)
+                         | Error e -> SubmitResult (Error e)
+                        )
+                let state =
+                    { emoji with
+                        EmojiTagsEditState =
+                            {| edit with
+                                Submit = InProgress
+                            |}
+                            |> Edit
+                    }
+                state, cmd
+            | View ->
+                state, Cmd.none
+        | SubmitResult(res) ->
+            match res with
+            | Ok emoji ->
+                let state =
+                    {
+                        Emoji = emoji
+                        EmojiTagsEditState = View
+                    }
+                state, Cmd.none
+            | Error err ->
+                let emoji = state
+                match emoji.EmojiTagsEditState with
+                | Edit edit ->
+                    let state =
+                        { emoji with
+                            EmojiTagsEditState =
+                                Edit {| edit with Submit = Resolved (Error err) |}
+                        }
+
+                    state, Cmd.none
+                | View ->
+                    state, Cmd.none
+        | InputTagsMsg( msg) ->
+            let emoji = state
+            match emoji.EmojiTagsEditState with
+            | Edit edit ->
+                let inputTagsState, cmd =
+                    InputTags.update msg edit.Tags
+                let state =
+                    { emoji with
+                        EmojiTagsEditState =
+                            Edit {| edit with Tags = inputTagsState |}
+                    }
+                let cmd = Cmd.map (fun msg -> InputTagsMsg msg) cmd
+                state, cmd
+            | View ->
+                state, Cmd.none
+
+    let view (emoji:EmojiState) dispatch =
+        let emoji' = emoji.Emoji
+        match emoji.EmojiTagsEditState with
+        | View ->
+            Tag.list [] [
+                for x in emoji'.Tags do
+                    Tag.tag [
+                    ] [
+                        a [Href (Router.format [TagRoute; x])] [str x]
+                    ]
+
+                Button.button [
+                    Button.OnClick (fun _ ->
+                        dispatch ChangeToEditMode
+                    )
+                ] [
+                    Fa.i [ Fa.Solid.Edit ] []
+                ]
+            ]
+
+        | Edit edit ->
+            match edit.Submit with
+            | HasNotStartedYet | Resolved _ ->
+                div [] [
+                    InputTags.view edit.Tags (fun msg ->
+                        InputTagsMsg msg
+                        |> dispatch)
+
+                    Button.button [
+                        let isDisabled =
+                            match edit.Submit with
+                            | Resolved(Error x) -> true
+                            | _ -> false
+                        if isDisabled then
+                            Button.Color Color.IsDanger
+                        Button.Disabled isDisabled
+                        Button.OnClick (fun _ ->
+                            if not isDisabled then
+                                dispatch Submit
+                        )
+                    ] [
+                        match edit.Submit with
+                        | Resolved(Error errorMsg) ->
+                            str (sprintf "%A" errorMsg)
+                        | _ ->
+                            Fa.i [ Fa.Solid.Check ] []
+                    ]
+
+                ]
+            | InProgress -> spinner
 
 type State =
     {
         EmojiUploadForm: EmojiUploadForm.State
         FindEmojisByTags: InputTags.State
-        EmojisResult: Deferred<Map<EmojiId, EmojiState>>
+        EmojisResult: Deferred<Map<EmojiId, EmojiTagsEdit.EmojiState>>
     }
 
 type Msg =
     | EmojiUploadFormMsg of EmojiUploadForm.Msg
     | InputTagsMsg of InputTags.Msg
+
     | Find
     | FindResult of Emoji list
-    | ChangeUrl of string list
 
-    | EmojiTagsEdit of EmojiId
-    | EmojiTagsSubmit of EmojiState
-    | EmojiTagsResult of EmojiId * Result<Emoji, unit>
-    | EmojiTagsInputTagsMsg of EmojiId * InputTags.Msg
-[<Literal>]
-let TagRoute = "tag"
+    | ChangeUrl of string list
+    | EmojiTagsEditMsg of EmojiId * EmojiTagsEdit.Msg
 
 let parseUrl state segments =
     match segments with
@@ -263,11 +402,8 @@ let update (msg: Msg) (state: State): State * Cmd<Msg> =
                 EmojisResult =
                     emojis
                     |> List.fold
-                        (fun st e ->
-                            let e' =
-                                { Emoji = e
-                                  EmojiTagsEditState = View }
-                            Map.add e.Id e' st
+                        (fun st emoji ->
+                            Map.add emoji.Id (EmojiTagsEdit.init emoji) st
                         )
                         Map.empty
                     |> Resolved }
@@ -276,120 +412,22 @@ let update (msg: Msg) (state: State): State * Cmd<Msg> =
     | ChangeUrl segments ->
         parseUrl state segments
 
-    | EmojiTagsEdit emojiId ->
-        let state =
-            match state.EmojisResult with
-            | Resolved xs ->
-                match Map.tryFind emojiId xs with
-                | Some emoji ->
-                    let emoji =
-                        { emoji with
-                            EmojiTagsEditState =
-                                {|
-                                    Tags = InputTags.init emoji.Emoji.Tags
-                                    Submit = HasNotStartedYet |}
-                                |> Edit
-                        }
-                    { state with
-                        EmojisResult =
-                            Resolved (Map.add emojiId emoji xs) }
-                | None -> state
-            | _ -> state
-        state, Cmd.none
-    | EmojiTagsSubmit emoji ->
-        match state.EmojisResult with
-        | Resolved emojis ->
-            match emoji.EmojiTagsEditState with
-            | Edit edit ->
-                let em =
-                    { emoji.Emoji with
-                        Tags = edit.Tags.InputTagsState.Tags
-                    }
-                let emojiId = emoji.Emoji.Id
-                let cmd =
-                    Cmd.OfAsync.perform
-                        api.updateEmoji
-                        em
-                        (function
-                         | Ok () -> EmojiTagsResult (emojiId, Ok em)
-                         | Error e -> EmojiTagsResult (emojiId, Error e)
-                        )
-                let emoji =
-                    { emoji with
-                        EmojiTagsEditState =
-                            {| edit with
-                                Submit = InProgress
-                            |}
-                            |> Edit
-                    }
-                let state =
-                    { state with
-                        EmojisResult =
-                            Resolved (Map.add emojiId emoji emojis) }
-                state, cmd
-            | View -> state, Cmd.none
-        | _ -> state, Cmd.none
-    | EmojiTagsResult(emojiId, res) ->
-        match state.EmojisResult with
-        | Resolved emojis ->
-            match res with
-            | Ok emoji ->
-                let emojiId = emoji.Id
-                let emoji =
-                    {
-                        Emoji = emoji
-                        EmojiTagsEditState = View
-                    }
-                let state =
-                    { state with
-                        EmojisResult =
-                            Resolved (Map.add emojiId emoji emojis) }
-                state, Cmd.none
-            | Error err ->
-                match Map.tryFind emojiId emojis with
-                | Some emoji ->
-                    match emoji.EmojiTagsEditState with
-                    | Edit edit ->
-                        let emoji =
-                            { emoji with
-                                EmojiTagsEditState =
-                                    Edit {| edit with Submit = Resolved (Error err) |}
-                            }
-                        let state =
-                            { state with
-                                EmojisResult =
-                                    Resolved (Map.add emojiId emoji emojis) }
-                        state, Cmd.none
-                    | View ->
-                        state, Cmd.none
-                | None ->
-                    state, Cmd.none
-        | _ -> state, Cmd.none
-    | EmojiTagsInputTagsMsg(emojiId, msg) ->
+    | EmojiTagsEditMsg(emojiId, msg) ->
         match state.EmojisResult with
         | Resolved emojis ->
             match Map.tryFind emojiId emojis with
-            | Some emoji ->
-                match emoji.EmojiTagsEditState with
-                | Edit edit ->
-                    let inputTagsState, cmd =
-                        InputTags.update msg edit.Tags
-                    let emoji =
-                        { emoji with
-                            EmojiTagsEditState =
-                                Edit {| edit with Tags = inputTagsState |}
-                        }
-                    let state =
-                        { state with
-                            EmojisResult =
-                                Resolved (Map.add emojiId emoji emojis) }
-
-                    let cmd = Cmd.map (fun msg -> EmojiTagsInputTagsMsg(emojiId, msg)) cmd
-                    state, cmd
-                | View -> failwith "Not Implemented"
-            | None -> failwith "Not Implemented"
-        | HasNotStartedYet -> failwith "Not Implemented"
-        | InProgress -> failwith "Not Implemented"
+            | Some emojiState ->
+                let emojiState, cmd =
+                    EmojiTagsEdit.update msg emojiState
+                let state =
+                    { state with
+                        EmojisResult =
+                            Resolved (Map.add emojiId emojiState emojis) }
+                let cmd = Cmd.map (fun msg -> EmojiTagsEditMsg(emojiId, msg)) cmd
+                state, cmd
+            | None ->
+                state, Cmd.none
+        | _ -> state, Cmd.none
 
 open Fable.React
 open Fable.React.Props
@@ -437,60 +475,18 @@ let containerBox (state : State) (dispatch : Msg -> unit) =
             | Resolved emojis ->
                 Content.content [] [
                     Content.Ol.ol [] [
-                        for KeyValue(emojiId, emoji) in emojis do
-                            let emoji' = emoji.Emoji
-                            match emoji.EmojiTagsEditState with
-                            | View ->
-                                Tag.list [] [
-                                    for x in emoji'.Tags do
-                                        Tag.tag [
-                                        ] [
-                                            a [Href (Router.format [TagRoute; x])] [str x]
-                                        ]
+                        for KeyValue(emojiId, emojiState) in emojis do
+                            EmojiTagsEdit.view emojiState (fun msg ->
+                                EmojiTagsEditMsg(emojiId, msg)
+                                |> dispatch)
 
-                                    Button.button [
-                                        Button.OnClick (fun _ ->
-                                            dispatch (EmojiTagsEdit emojiId)
-                                        )
-                                    ] [
-                                        Fa.i [ Fa.Solid.Edit ] []
-                                    ]
-                                ]
-
-                            | Edit edit ->
-                                match edit.Submit with
-                                | HasNotStartedYet | Resolved _ ->
-                                    InputTags.view edit.Tags (fun msg ->
-                                        EmojiTagsInputTagsMsg(emojiId, msg)
-                                        |> dispatch)
-                                    Button.button [
-                                        let isDisabled =
-                                            match edit.Submit with
-                                            | Resolved(Error x) -> true
-                                            | _ -> false
-                                        if isDisabled then
-                                            Button.Color Color.IsDanger
-                                        Button.Disabled isDisabled
-                                        Button.OnClick (fun _ ->
-                                            if not isDisabled then
-                                                dispatch (EmojiTagsSubmit emoji)
-                                        )
-                                    ] [
-                                        match edit.Submit with
-                                        | Resolved(Error errorMsg) ->
-                                            str (sprintf "%A" errorMsg)
-                                        | _ ->
-                                            Fa.i [ Fa.Solid.Check ] []
-                                    ]
-                                | InProgress -> spinner
-
-                            img [ Src emoji'.Id ]
+                            img [ Src emojiId ]
 
                             match Browser.Navigator.navigator.clipboard with
                             | Some clipboard ->
                                 Button.span [
                                     Button.OnClick (fun _ ->
-                                        clipboard.writeText emoji'.Id
+                                        clipboard.writeText emojiId
                                         |> ignore
                                     )
                                 ] [
